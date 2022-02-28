f7a1e360a6533839c18c705eb59d5683cef5c096
Thái Trần <thaitv.itk35@gmail.com>
2022-02-28 08:40:35 +0700

    Update code

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,22 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  
+  const router = useRouter();
+
+  const goToDetailPage = () => {
+    router.push({
+      pathname: '/posts/[postId]',
+      query: {
+        postId: 123,
+        ref: 'social',
+      },
+    })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -22,8 +34,10 @@ const Home: NextPage = () => {
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
+        <div style={{ marginTop: '2000px'}}></div>
+        <Link href='/about'><a>Go to about</a></Link>
 
-       <Link href=''><a>Go to about</a></Link>
+        <button onClick={goToDetailPage}>Go to post detail page</button>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
