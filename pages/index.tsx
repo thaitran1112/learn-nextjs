92ab64cdcad8341669707dbf42de4e15970eedbe
Thái Trần <thaitv.itk35@gmail.com>
2022-03-02 00:24:41 +0700

    Update code

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,6 @@ const Home: NextPage = () => {
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
-        <div style={{ marginTop: '2000px'}}></div>
         <Link href='/about'><a>Go to about</a></Link>
 
         <button onClick={goToDetailPage}>Go to post detail page</button>
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import _isArray from 'lodash/isArray';
+import _map from 'lodash/map';
+import { GetStaticProps, GetStaticPropsContext } from 'next';
+
+export interface PostPageProps {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 export interface PostListPageProps {
+  posts: PostPageProps[];
+}
+
+export default function PostListPage({ posts }: PostListPageProps){
+  return (
+    <div>
+      <h1>Post List Page</h1>
+      <ul>
+        {_isArray(posts) && _map(posts, post => (
+           <li key={post.id}>{post?.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+
 
 }
 
-export default function PostListPage(props: PostListPageProps){
-  return <div>Post List Page</div>
+
+export const getStaticProps: GetStaticProps<PostListPageProps> = async (context: GetStaticPropsContext) => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts: [...posts]
+    },
+  }
 }
